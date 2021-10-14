3478d6beae027ef6578be28c4a941b16bb0a7ab3
Isatou K Njie <isatoukn696@gmail.com>
2021-10-14 11:59:40 +0000

    first push on flutter

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Be yourself;',
+    'There is nothing better than being you always.',
+    'You are beautiful and special so love yourself.'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: quotes.map((quote){
+          return Text(quote);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+
+
